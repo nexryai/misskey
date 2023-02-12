d319fad52218b96837ee46d03e6501158981e6ba
nexryai <gnomer@tuta.io>
2023-02-12 19:14:30 +0900

    use freasearch

--- packages/frontend/src/components/MkGoogle.vue
+++ packages/frontend/src/components/MkGoogle.vue
@@ -15,7 +15,7 @@ const props = defineProps<{
 const query = ref(props.q);
 
 const search = () => {
-	window.open(`https://www.google.com/search?q=${query.value}`, '_blank');
+	window.open(`https://freasearch.org/search?q=${query.value}`, '_blank');
 };
 </script>
 
--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -206,7 +206,7 @@ export const defaultStore = markRaw(new Storage('base', {
 	},
 	showGapBetweenNotesInTimeline: {
 		where: 'device',
-		default: false,
+		default: true,
 	},
 	darkMode: {
 		where: 'device',
