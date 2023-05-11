8d462e78fb6dd3568b137cbe487925b15558a1a3
nexryai <git@nexryai.online>
2023-05-11 21:38:01 +0900

    Update about-misskey

--- packages/frontend/src/pages/about-misskey.vue
+++ packages/frontend/src/pages/about-misskey.vue
@@ -29,16 +29,6 @@
 							{{ i18n.ts._aboutMisskey.source }}
 							<template #suffix>GitHub</template>
 						</FormLink>
-						<FormLink to="https://crowdin.com/project/misskey" external>
-							<template #icon><i class="ti ti-language-hiragana"></i></template>
-							{{ i18n.ts._aboutMisskey.translation }}
-							<template #suffix>Crowdin</template>
-						</FormLink>
-						<FormLink to="https://www.patreon.com/syuilo" external>
-							<template #icon><i class="ti ti-pig-money"></i></template>
-							{{ i18n.ts._aboutMisskey.donate }}
-							<template #suffix>Patreon</template>
-						</FormLink>
 					</div>
 				</FormSection>
 				<FormSection>
